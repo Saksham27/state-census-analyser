0ee2df1966802e11f8382619ffc924af4dff1712
Saksham27 <geekysaksham27@gmail.com>
2020-04-21 13:17:36 +0530

    Added: State census analyser and method to read record

---
+++ StateCensusAnalyzer/CensusAnalyserException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StateCensusAnalyzer
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    class CensusAnalyserException : Exception
+    {
+        public enum CensusExceptionType
+        {
+            file_not_found
+        }
+
+        //variables
+        CensusExceptionType exceptionType;
+
+        /// <summary>
+        /// exception method
+        /// </summary>
+        /// <param name="exceptionType"> exception type </param>
+        /// <param name="exceptionMessage"> exception message </param>
+        public CensusAnalyserException(CensusExceptionType exceptionType, string exceptionMessage) : base(exceptionMessage)
+        {
+            this.exceptionType = exceptionType;
+        }
+
+    }
+}
---
+++ StateCensusAnalyzer/StateCensusAnalyser.cs
@@ -0,0 +1,53 @@
+﻿using LumenWorks.Framework.IO.Csv;
+using System;
+using System.IO;
+using System.Text;
+
+namespace StateCensusAnalyzer
+{
+    class StateCensusAnalyser
+    {
+
+        int numberOfRecords = default;
+        string file;
+
+        /// <summary>
+        /// constructor with string parameter
+        /// </summary>
+        /// <param name="file"> path of file </param>
+        public StateCensusAnalyser(string file)
+        {   
+            this.file = file;
+        }
+
+        public void ReadRecords()
+        {
+            try
+            {
+                using CsvReader records = new CsvReader(new StreamReader(this.file));
+                while (records.ReadNextRecord())
+                {
+                    this.numberOfRecords++;
+                }
+            }
+            catch(FileNotFoundException)
+            {
+                throw new CensusAnalyserException(CensusAnalyserException.CensusExceptionType.file_not_found, "Wrong file path or file missing");
+            }
+            catch(Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+
+        /// <summary>
+        /// main method
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+
+        }
+
+    }
+}
