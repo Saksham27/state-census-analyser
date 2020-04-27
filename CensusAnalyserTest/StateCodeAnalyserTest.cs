6409f2a9bdc512915d00d196ba7c789033881d55
Saksham27 <geekysaksham27@gmail.com>
2020-04-28 03:44:26 +0530

    Test case 2.1 - Read no fo records

---
+++ CensusAnalyserTest/StateCodeAnalyserTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CensusAnalyser;
+using NUnit.Framework;
+
+namespace CensusAnalyserTest
+{
+    [TestFixture]
+    class StateCodeAnalyserTest
+    {
+        /// <summary>
+        /// Test to check number of records
+        /// </summary>
+        [Test]
+        public void TestNumberOfRecords()
+        {
+            // create object to StateCodeAnalysis 
+            StateCodeAnalyser stateCodeAnalyser = new StateCodeAnalyser();
+            // call read records method to load and read file
+            stateCodeAnalyser.ReadRecords();
+            // call numberof records method to get total records number
+            var records = stateCodeAnalyser.NumberOfRecords();
+            Assert.AreEqual(37, records);
+        }//// end : public void TestNumberOfRecords()
+    }//// end : class StateCodeAnalyserTest
+}//// end : namespace CensusAnalyserTest
---
+++ StateCensusAnalyzer/StateCodeAnalyser.cs
@@ -0,0 +1,53 @@
+﻿namespace CensusAnalyser
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.IO;
+    using LumenWorks.Framework.IO.Csv;
+    using StateCensusAnalyzer;
+
+    public class StateCodeAnalyser
+    {
+        // read only path variable
+        readonly string filepath;
+        // variable to count numberof variables
+        int recordsCount;
+
+        public StateCodeAnalyser(string path = @"C:\Users\Saksham\source\repos\StateCensusAnalyzer\StateCode.csv")
+        {
+            this.filepath = path;
+        }
+
+        /// <summary>
+        /// Method to read the records in the file
+        /// </summary>
+        public void ReadRecords()
+        {
+            try
+            {
+                using (CsvReader State_code_reader = new CsvReader(new StreamReader(filepath)))
+                {
+                    // loop and count the number of records in the file
+                    while (State_code_reader.ReadNextRecord()) recordsCount++;
+                }
+            }
+            catch (ExceptionFileNotFound exceptionMessage)
+            {
+                Console.WriteLine(exceptionMessage.Message);
+            }
+            catch (Exception exceptionMessage)
+            {
+                Console.WriteLine(exceptionMessage.Message);
+            }
+        }
+        /// <summary>
+        /// Method to return total number of records in the given file
+        /// </summary>
+        /// <returns></returns>
+        public int NumberOfRecords()
+        {
+            return recordsCount;
+        }
+    }
+}
